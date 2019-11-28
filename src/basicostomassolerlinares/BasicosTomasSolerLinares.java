77c67a49531411fb18ae7c59c02f00d2f9ea3dd2
Tomás Soler <tomas@correo.com>
2019-11-28 10:23:15 +0100

    Rama numericos lista

--- src/basicostomassolerlinares/BasicosTomasSolerLinares.java
+++ src/basicostomassolerlinares/BasicosTomasSolerLinares.java
@@ -12,10 +12,31 @@ package basicostomassolerlinares;
 public class BasicosTomasSolerLinares {
 
     /**
-     * @param args the command line arguments
+     * Método para mostrar ejemplos de tipos numericos.
      */
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;
+        short chiquitin;
+        int entero;
+        long duplicaEntero;
+        float grande;
+        double doble;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.err.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        numericos();
     }
     
 }
